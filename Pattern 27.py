962e7b6a018237634f463825dae546787f3cd874
shubham5996 <67471357+shubham5996@users.noreply.github.com>
2020-06-30 14:28:54 +0530

    Updated file

--- Pattern 27.py
+++ Pattern 27.py
@@ -1,12 +1,17 @@
+# The function which makes and prints the matrix
 def make_matrix(number):
 	global output, count
+	# Determining number of sides
 	side = 1 + (number-1)*2
+	# Creating a skeleton matrix with the given number as every element
 	output = [[number]*side for x in range(side)]
+	# Determining where to stop after building half the matrix
 	mid = side/2 + 1/2
 	count = 0
 	def do():
 		global output, count
 		new_list = []
+		# alters the skeletal matrix to assign values as needed
 		for a,row in enumerate(output, 0):
 			rows = []
 			if a == (side/2 + 1/2):
@@ -19,12 +24,14 @@ def make_matrix(number):
 				else:
 					rows.append(number)
 			new_list.append(rows)
+		# makes a final matrix by adding half the list to itself
 		new_list += new_list[-2::-1]
 		output = new_list
-	
+	# increases count to change values as you move towards center
 	for n in range(number):
 		do()
 		count+=1
+	# prints matrix in a readable format
 	for x in output:
 		print(' '.join([str(element) for element in x]))
 
